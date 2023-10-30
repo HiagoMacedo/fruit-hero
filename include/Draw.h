7c9e0e9ae87dde31020b058125d41a79f442fe3f
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2023-10-30 14:46:25 -0300

    Adicionado classe Draw e haarcascade

---
+++ include/Draw.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include "UniformRandomInt.h"
+#include "Fruit.h"
+#include "Game.h"
+#include "Audio.h"
+#include <vector>
+#include <string>
+#include <filesystem>
+
+using namespace cv;
+
+
+class Draw
+{
+public:
+    Draw(std::string sequenceFile);
+
+    void drawTransparency(Mat frame, Mat transp, int xPos, int yPos);
+
+    void drawTransRect(Mat frame, Scalar color, double alpha, Rect region);
+
+    void detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryflip, Audio& audio);
+    
+    void drawFruit(Mat& smallImg, Fruit& f, Audio& audio);
+
+    void drawLanes(Mat& smallImg);
+
+    bool containsFruit(Rect& r, Fruit& f, Audio& audio);
+
+    void updatePoints(Fruit& f);
+
+    int getPoints();
+
+    int getNextSequence();
+
+    void setCurrentLane(int cl);
+
+private:
+    int currentLane;
+    int points;
+    Fruit pear;
+    Fruit orange;
+    Fruit apple;
+    Fruit grape;
+    std::vector<std::string> sequence;
+    int sequenceHead;
+    std::filesystem::path sequencePath;
+    UniformRandomInt randomNumGen;
+};
---
+++ src/Draw.cpp
@@ -0,0 +1,198 @@
+#include "Draw.h"
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include "Gerenciador.h"
+
+using namespace std;
+
+const int MIN_LANE = 0;
+const int MAX_LANE = 3;
+
+const int FRUIT_SPEED = 7;
+
+const int ORANGE_LANE = 0;
+const int PEAR_LANE = 1;
+const int GRAPE_LANE = 2;
+const int APPLE_LANE = 3;
+
+/*
+Rect orange = (10,150,orange.cols,orange.rows);
+if (r && orange.area() > 50) // detecta
+*/
+
+Draw::Draw(std::string sequenceFile) 
+{
+    // std::filesystem::path sequence;
+    sequencePath = std::filesystem::current_path() / "sequences" / sequenceFile;
+    Gerenciador ge(sequencePath.string());
+    sequence = ge.read();
+    sequenceHead = 0;
+    randomNumGen.set(MIN_LANE, MAX_LANE);
+    currentLane = randomNumGen.get();
+    points = 0;
+    
+    orange = Fruit( imread("img/orange.png", IMREAD_UNCHANGED) );
+    pear = Fruit( imread("img/pears_resized.png", IMREAD_UNCHANGED) );
+    apple = Fruit( imread("img/apple_resized.png", IMREAD_UNCHANGED) );
+    grape = Fruit(imread("img/grape_resized.png", IMREAD_UNCHANGED) );
+    
+    orange.setX(80);
+    pear.setX(250);
+    grape.setX(400);
+    apple.setX(530);
+    
+}
+
+void Draw::drawTransparency(Mat frame, Mat transp, int xPos, int yPos) {
+    Mat mask;
+    vector<Mat> layers;
+
+    split(transp, layers); // seperate channels
+    Mat rgb[3] = { layers[0],layers[1],layers[2] };
+    mask = layers[3]; // png's alpha channel used as mask
+    merge(rgb, 3, transp);  // put together the RGB channels, now transp insn't transparent 
+    transp.copyTo(frame.rowRange(yPos, yPos + transp.rows).colRange(xPos, xPos + transp.cols), mask);
+
+}
+
+void Draw::drawTransRect(Mat frame, Scalar color, double alpha, Rect region) {
+    Mat roi = frame(region);
+    Mat rectImg(roi.size(), CV_8UC3, color); 
+    addWeighted(rectImg, alpha, roi, 1.0 - alpha , 0, roi); 
+
+}
+
+void Draw::drawFruit(cv::Mat& smallImg, Fruit& f, Audio& audio) {
+    drawTransparency(smallImg, f.getImage(), f.getX(), f.getY());
+
+    if (f.getY() < (smallImg.rows - f.getImage().rows)-FRUIT_SPEED) {
+        f.incrementY(FRUIT_SPEED);
+    } 
+    else {
+        audio.playMiss();
+        f.setY(0);
+        // currentLane = randomNumGen.get();
+        currentLane = getNextSequence();
+    }
+}
+
+void Draw::detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryflip, Audio& audio) {
+    double t = 0;
+    vector<Rect> faces;
+    Mat gray, smallImg;
+    Scalar color = Scalar(255,0,0);
+
+    double fx = 1 / scale;
+    resize( img, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT );
+    if( tryflip )
+        flip(smallImg, smallImg, 1);
+    cvtColor( smallImg, gray, COLOR_BGR2GRAY );
+    equalizeHist( gray, gray );
+
+    t = (double)getTickCount();
+
+    cascade.detectMultiScale( gray, faces,
+        1.3, 2, 0
+        //|CASCADE_FIND_BIGGEST_OBJECT
+        //|CASCADE_DO_ROUGH_SEARCH
+        |CASCADE_SCALE_IMAGE,
+        Size(70, 70) );
+    t = (double)getTickCount() - t;
+    // printf( "detection time = %g ms\n", t*1000/getTickFrequency());
+
+    // Desenha uma imagem
+    if (currentLane == ORANGE_LANE) {
+        // std::cout << "Fruit: Orange\n";
+        drawFruit(smallImg, orange, audio);
+    }
+    if (currentLane == PEAR_LANE) {
+        // std::cout << "Fruit: Pear\n";
+        drawFruit(smallImg, pear, audio);
+    }
+    if(currentLane == GRAPE_LANE) {
+        // std::cout << "Fruit: Grape\n";
+        drawFruit(smallImg, grape, audio);
+    }
+    if (currentLane == APPLE_LANE) {
+        // std::cout << "Fruit: Apple\n";
+        drawFruit(smallImg, apple, audio);
+    }
+
+    // PERCORRE AS FACES ENCONTRADAS
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Rect r = faces[i];
+        rectangle( smallImg, Point(cvRound(r.x), cvRound(r.y)),
+                    Point(cvRound((r.x + r.width-1)), cvRound((r.y + r.height-1))),
+                    color, 3);
+
+        if (currentLane == ORANGE_LANE) {
+            containsFruit(faces[0], orange, audio);
+            break;
+        }
+        if (currentLane == PEAR_LANE) {
+            containsFruit(faces[0], pear, audio);
+            break;
+        }
+        if(currentLane == GRAPE_LANE) {
+            containsFruit(faces[0], grape, audio);
+            break;
+        }
+        if(currentLane == APPLE_LANE) {
+            containsFruit(faces[0], apple, audio);
+            break;
+        }
+    }
+
+    drawLanes(smallImg);
+
+    // Desenha um texto
+    color = Scalar(0,0,255);
+    putText	(smallImg, "Points: " + to_string(points), Point(10, 30), FONT_HERSHEY_PLAIN, 2, color, 2); // fonte
+
+    // Desenha o frame na tela
+    imshow("Fruit Hero", smallImg );
+    // printf("image::width: %d, height=%d\n", smallImg.cols, smallImg.rows );
+
+}
+
+void Draw::drawLanes(Mat& smallImg) {
+    double alpha = 0.3; 
+
+    drawTransRect(smallImg, Scalar(0,255,255), alpha, Rect(100, 0, 80, smallImg.rows));
+    drawTransRect(smallImg, Scalar(0,255,0), alpha, Rect(260, 0, 80, smallImg.rows));
+    drawTransRect(smallImg, Scalar(255,0,0), alpha, Rect(400, 0, 80, smallImg.rows));
+    drawTransRect(smallImg, Scalar(0,0,255), alpha, Rect(540, 0, 80, smallImg.rows));
+}
+
+bool Draw::containsFruit(Rect& r, Fruit& f, Audio& audio) {
+    // if(r && f.getRectangle().area() > 60) {
+    if( r.contains( cv::Point(f.getX(), f.getY()) ) ) {
+        audio.playCorrect();
+        updatePoints(f);
+        return true;
+    }
+    return false;
+}
+
+void Draw::updatePoints(Fruit& f) {
+    points++;
+    f.setY(0);
+    // currentLane = randomNumGen.get();
+    currentLane = getNextSequence();
+}
+
+int Draw::getPoints() {
+    return points;
+}
+
+int Draw::getNextSequence() {
+    int next = stoi(sequence[sequenceHead]);
+    sequenceHead++;
+    return next;
+}
+
+void Draw::setCurrentLane(int cl) {
+    currentLane = cl;
+}
