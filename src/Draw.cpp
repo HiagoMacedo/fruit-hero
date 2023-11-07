bbb28f3b72342bcaf160ba8524cdae752a5ea628
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2023-11-07 09:12:42 -0300

    Mudado valor de 'alpha' em drawLanes

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -141,7 +141,7 @@ void Draw::detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bo
     // Desenha um texto
     color = Scalar(0,0,255);
     // putText	(smallImg, "Points: " + to_string(points), Point(10, 30), FONT_HERSHEY_PLAIN, 2, color, 2); // fonte
-    putText	(smallImg, to_string(points.getPoints()), Point(20, 30), FONT_HERSHEY_PLAIN, 2, color, 2); // fonte
+    putText	(smallImg, to_string(points.getPoints()), Point(40, 30), FONT_HERSHEY_PLAIN, 2, color, 2); // fonte
 
 
     // Desenha o frame na tela
@@ -150,7 +150,7 @@ void Draw::detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bo
 }
 
 void Draw::drawLanes(Mat& smallImg) {
-    double alpha = 0.4; 
+    double alpha = 0.3; 
 
     drawTransRect(smallImg, Scalar(0,0,0), 1.0, Rect(0, 0, 100, smallImg.rows));
     drawTransRect(smallImg, Scalar(0,255,255), alpha, Rect(100, 0, 140, smallImg.rows));
